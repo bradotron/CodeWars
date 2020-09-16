3ccb48197ce6d6efdb2634de0f90ce1dda40f52e
Bradley Jensen <bradley13@gmail.com>
2020-09-16 08:35:06 -0400

    DescendingOrder passed

---
+++ 20200916/DescendingOrder.Tests/DescendingOrder_IsDescendingShould.cs
@@ -0,0 +1,26 @@
+using NUnit.Framework;
+using DescendingOrder;
+
+namespace DescendingOrder.Tests
+{
+    [TestFixture]
+    public class Tests
+    {
+        private DescendingOrder _descendingOrder;
+
+        [SetUp]
+        public void Setup()
+        {
+            _descendingOrder = new DescendingOrder();
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.AreEqual(0, DescendingOrder.PutIntoDescendingOrder(0));   
+            Assert.AreEqual(54421, DescendingOrder.PutIntoDescendingOrder(42145));
+            Assert.AreEqual(654321, DescendingOrder.PutIntoDescendingOrder(145263));
+            Assert.AreEqual(987654321, DescendingOrder.PutIntoDescendingOrder(123456789));
+        }
+    }
+}
--- 20200916/DescendingOrder/DescendingOrder.cs
+++ 20200916/DescendingOrder/DescendingOrder.cs
@@ -1,8 +1,27 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace DescendingOrder
 {
-    public class Class1
+  public class DescendingOrder
+  {
+    public static int PutIntoDescendingOrder(int num)
     {
+      string result = "";
+      string numberString = num.ToString();
+      List<int> digits = new List<int>();
+      foreach(char c in numberString) {
+          digits.Add((int)Char.GetNumericValue(c));
+      }
+      digits.Sort(delegate (int x, int y)
+      {
+        return y > x ? 1 : -1;
+      });
+      foreach (int digit in digits)
+      {
+        result += digit;
+      }
+      return int.Parse(result);
     }
+  }
 }
