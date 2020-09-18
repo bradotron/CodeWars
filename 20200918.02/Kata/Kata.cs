f4f23605b5057c187bbec399745cec5064f558ce
Bradley Jensen <bradley13@gmail.com>
2020-09-18 14:23:03 -0400

    SpinWords

---
+++ 20200918.02/Kata/Kata.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Kata
+{
+  public class Kata
+  {
+    public static string SpinWords(string sentence)
+    {
+      string result = "";
+      string[] words = sentence.Split(' ');
+
+      foreach (string word in words)
+      {
+        if (word.Length > 4)
+        {
+          char[] chars = word.ToCharArray();
+          Array.Reverse(chars);
+          result += new string(chars);
+        }
+        else
+        {
+          result += word;
+        }
+        result += " ";
+      }
+
+      return result.Trim();
+    }
+  }
+}
