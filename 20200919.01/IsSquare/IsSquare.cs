529836b828d98a0b01a22e3ca012d16ce3fa6f11
Bradley Jensen <bradley13@gmail.com>
2020-09-19 10:55:04 -0400

    IsSquare

---
+++ 20200919.01/IsSquare/IsSquare.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace IsSquare
+{
+  public class Kata
+  {
+    public static bool IsSquare(int n)
+    {
+        //return Math.Sqrt(n) % 1 == 0;
+      if (n < 0)
+      {
+        return false;
+      }
+      else
+      {
+        int sqrt = (int)Math.Sqrt(n);
+        return sqrt * sqrt == n;
+      }
+    }
+  }
+}
