9709b52a3dc3c57f9dad0b7012d2cf2dc06d05c3
Bradley Jensen <bradley13@gmail.com>
2020-09-19 11:43:26 -0400

    Accum

---
+++ 20200919.02/Kata.Tests/UnitTest1.cs
@@ -0,0 +1,19 @@
+using NUnit.Framework;
+using Kata;
+
+namespace Kata.Tests
+{
+  [TestFixture]
+  public class Tests
+  {
+    [Test]
+    public void Test1()
+    {
+      Assert.AreEqual(Kata.Accum("ZpglnRxqenU"), "Z-Pp-Ggg-Llll-Nnnnn-Rrrrrr-Xxxxxxx-Qqqqqqqq-Eeeeeeeee-Nnnnnnnnnn-Uuuuuuuuuuu");
+      Assert.AreEqual(Kata.Accum("NyffsGeyylB"), "N-Yy-Fff-Ffff-Sssss-Gggggg-Eeeeeee-Yyyyyyyy-Yyyyyyyyy-Llllllllll-Bbbbbbbbbbb");
+      Assert.AreEqual(Kata.Accum("MjtkuBovqrU"), "M-Jj-Ttt-Kkkk-Uuuuu-Bbbbbb-Ooooooo-Vvvvvvvv-Qqqqqqqqq-Rrrrrrrrrr-Uuuuuuuuuuu");
+      Assert.AreEqual(Kata.Accum("EvidjUnokmM"), "E-Vv-Iii-Dddd-Jjjjj-Uuuuuu-Nnnnnnn-Oooooooo-Kkkkkkkkk-Mmmmmmmmmm-Mmmmmmmmmmm");
+      Assert.AreEqual(Kata.Accum("HbideVbxncC"), "H-Bb-Iii-Dddd-Eeeee-Vvvvvv-Bbbbbbb-Xxxxxxxx-Nnnnnnnnn-Cccccccccc-Ccccccccccc");
+    }
+  }
+}
---
+++ 20200919.02/Kata/Kata.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Kata
+{
+  public class Kata
+  {
+    public static String Accum(string s)
+    {
+      char[] chars = s.ToLower().ToCharArray();
+      string result = "";
+      for (int i = 0; i < chars.Length; i++)
+      {
+        result += chars[i].ToString().ToUpper();
+
+        int j = 0;
+        while (j < i)
+        {
+          result += chars[i].ToString();
+          j++;
+        }
+
+        result += "-";
+      }
+      return result.Trim('-');
+    }
+  }
+}
