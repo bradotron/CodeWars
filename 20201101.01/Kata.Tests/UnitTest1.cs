8fa018807635283c4b45c85ba9bcf1bb6595e3c5
Bradley Jensen <bradley13@gmail.com>
2020-11-01 13:43:53 -0500

    Least Common Denominator - In Progress

---
+++ 20201101.01/Kata.Tests/UnitTest1.cs
@@ -0,0 +1,26 @@
+using NUnit.Framework;
+using System;
+
+namespace Kata.Tests
+{
+  public class Tests
+  {
+    [Test]
+    public void Test1()
+    {
+      long[,] lst = new long[,] { { 1, 2 }, { 1, 3 }, { 1, 4 } };
+      Assert.AreEqual("(6,12)(4,12)(3,12)", Fracts.convertFrac(lst));
+    }
+
+    [Test]
+    public void Test2()
+    {
+      //(69,130)(87,1310)(3,4)
+      // Expected: "(18078,34060)(2262,34060)(25545,34060)"
+      // But was:  "(90390,170300)(11310,170300)(127725,170300)"
+
+      long[,] lst = new long[,] { { 69, 130 }, { 87, 1310 }, { 3, 4 } };
+      Assert.AreEqual("(18078,34060)(2262,34060)(25545,34060)", Fracts.convertFrac(lst));
+    }
+  }
+}
---
+++ 20201101.01/Kata/Kata.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace Kata
+{
+  public class Fracts
+  {
+    public static string convertFrac(long[,] lst)
+    {
+      ////
+      // change all this to find least common multiple of the denominators, then just multiply the numerators by LCM / denominator
+
+
+      return output;
+    }
+  }
+}
