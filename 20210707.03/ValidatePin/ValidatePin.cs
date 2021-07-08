a104bd72e535327c3301cd71a3f40933f9664024
Bradley Jensen <bradley13@gmail.com>
2021-07-07 20:09:45 -0400

    fun with regex

---
+++ 20210707.03/ValidatePin/ValidatePin.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace ValidatePin
+{
+  public class Kata
+  {
+    public static bool ValidatePin(string pin)
+    {
+      Regex onlyNumbers = new Regex(@"^[0-9]*$");
+
+      if (pin.Contains("\n"))
+      {
+        return false;
+      }
+
+      if ((pin.Length == 4 || pin.Length == 6) && onlyNumbers.IsMatch(pin))
+      {
+        return true;
+      }
+
+      return false;
+    }
+  }
+}
