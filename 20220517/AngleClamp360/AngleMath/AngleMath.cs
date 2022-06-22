f8ab3ac2b3ae8d628a54f7c50d7e12704a715f76
Bradley Jensen <bradley13@gmail.com>
2022-06-22 08:42:35 -0400

    some angle math testing for unity project

---
+++ 20220517/AngleClamp360/AngleMath/AngleMath.cs
@@ -0,0 +1,18 @@
+﻿namespace AngleMath;
+public class AngleMath
+{
+  public static float AngleClamp360(float angle)
+  {
+    if (angle > 360f)
+    {
+      return angle % 360f;
+    }
+
+    if (angle < 0f)
+    {
+      return 360f + (angle % 360f);
+    }
+
+    return angle;
+  }
+}
