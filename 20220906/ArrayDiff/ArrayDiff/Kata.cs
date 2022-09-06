7f988bb9a8fc53ba04bf31ec8c0fe02787eaecd7
Bradley Jensen <bradley13@gmail.com>
2022-09-06 15:39:27 -0400

    ArrayDiff - collection assert is important with MSTest

---
+++ 20220906/ArrayDiff/ArrayDiff/Kata.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Kata
+{
+  public static int[] ArrayDiff(int[] a, int[] b)
+  {
+    Console.WriteLine(ArrayToString(a) + " - " + ArrayToString(b));
+    HashSet<int> hashB = new HashSet<int>(b);
+    List<int> diff = new List<int>();
+
+    foreach (int i in a)
+    {
+      if (!hashB.Contains(i))
+      {
+        diff.Add(i);
+      }
+    }
+    Console.WriteLine(ArrayToString(diff.ToArray()));
+    return diff.ToArray();
+  }
+
+  public static string ArrayToString(int[] array)
+  {
+    return "[ " + String.Join(", ", array) + " ]";
+  }
+}
---
+++ 20220906/ArrayDiff/ArrayDiffTests/KataTests.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests
+{
+  [TestClass()]
+  public class KataTests
+  {
+    [TestMethod()]
+    public void Test1()
+    {
+      CollectionAssert.AreEqual(new int[] { 2 }, Kata.ArrayDiff(new int[] { 1, 2 }, new int[] { 1 }));
+    }
+
+    [TestMethod()]
+    public void Test2()
+    {
+      CollectionAssert.AreEqual(new int[] { 2, 2 }, Kata.ArrayDiff(new int[] { 1, 2, 2 }, new int[] { 1 }));
+    }
+
+    [TestMethod()]
+    public void Test3()
+    {
+      CollectionAssert.AreEqual(new int[] { 1 }, Kata.ArrayDiff(new int[] { 1, 2, 2 }, new int[] { 2 }));
+    }
+
+    [TestMethod()]
+    public void Test4()
+    {
+      CollectionAssert.AreEqual(new int[] { 1, 2, 2 }, Kata.ArrayDiff(new int[] { 1, 2, 2 }, new int[] { }));
+    }
+
+    [TestMethod()]
+    public void Test5()
+    {
+      CollectionAssert.AreEqual(new int[] { }, Kata.ArrayDiff(new int[] { }, new int[] { 1, 2 }));
+    }
+
+    [TestMethod()]
+    public void Test6()
+    {
+      CollectionAssert.AreEqual(new int[] { 3 }, Kata.ArrayDiff(new int[] { 1, 2, 3 }, new int[] { 1, 2 }));
+    }
+  }
+}
