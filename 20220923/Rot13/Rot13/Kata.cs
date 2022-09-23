61cc908bcf213b7b2cc5359e6c586057c8cf6478
Bradley Jensen <bradley13@gmail.com>
2022-09-23 12:44:33 -0400

    fun rotation cypher

---
+++ 20220923/Rot13/Rot13/Kata.cs
@@ -0,0 +1,29 @@
+﻿public class Kata
+{
+  public static string Rot13(string message)
+  {
+    string cypher = String.Empty;
+
+    string letters = "abcdefghijklmnopqrstuvwxyz";
+    Dictionary<char, char> rot13 = new Dictionary<char, char>();
+    for (int i = 0; i < letters.Length; i++)
+    {
+      rot13.Add(letters[i], letters[(i + 13) % letters.Length]);
+      rot13.Add(Char.ToUpper(letters[i]), Char.ToUpper(letters[(i + 13) % letters.Length]));
+    }
+
+    foreach (char c in message)
+    {
+      if (rot13.ContainsKey(c))
+      {
+        cypher += rot13[c].ToString();
+      }
+      else
+      {
+        cypher += c.ToString();
+      }
+    }
+
+    return cypher;
+  }
+}
